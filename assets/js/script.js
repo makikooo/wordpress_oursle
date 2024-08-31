56e60a55620c613f20265ad0bdf14f3a1f774b8f
MAKIKO KARASAWA <truehope320@gmail.com>
2024-08-31 13:07:24 +0900

    診断ページ　CSS調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 //アコーディオンをクリックした時の動作
 $('.title').on('click', function() {//タイトル要素をクリックしたら
-  $('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
-    
   var findElm = $(this).next(".box");//タイトル直後のアコーディオンを行うエリアを取得
-    
+
   if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
     $(this).removeClass('close');//クラス名を除去    
+    $(findElm).slideUp(500);//アコーディオンを閉じる
   }else{//それ以外は
-    $('.close').removeClass('close'); //クラス名closeを全て除去した後
     $(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
     $(findElm).slideDown(500);//アコーディオンを開く
   }
@@ -23,6 +21,8 @@ $(window).on('load', function(){
     $(Box).slideDown(500);          //アコーディオンを開く
   });
 });
+
+
 
 //値をグラフに表示させる
 Chart.plugins.register({
@@ -240,3 +240,7 @@ $(window).resize(function() {
 $(window).on('load',function(){
   mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
 });
+
+
+
+
